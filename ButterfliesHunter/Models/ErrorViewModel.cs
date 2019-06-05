c11e27b20699901eef640ca474e84503c470a084
COB03 <COB03@LYTC-DEV03>
2019-06-05 14:07:41 +0100

    Add project files.

---
+++ ButterfliesHunter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ButterfliesHunter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
