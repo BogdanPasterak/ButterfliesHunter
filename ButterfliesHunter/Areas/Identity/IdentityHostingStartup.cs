5117d6b82121bcfafb5bfa538a3c14fdb54b5f63
COB03 <COB03@LYTC-DEV03>
2019-06-07 09:54:47 +0100

    Login cd

---
+++ ButterfliesHunter/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ButterfliesHunter.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ButterfliesHunter.Areas.Identity.IdentityHostingStartup))]
+namespace ButterfliesHunter.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
