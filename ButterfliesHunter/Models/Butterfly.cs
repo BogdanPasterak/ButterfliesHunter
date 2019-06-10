27f2ccf70abf30d4cac2b6552b4126c68ae95b1b
COB03 <COB03@LYTC-DEV03>
2019-06-10 14:35:27 +0100

    Add Butterfly

---
+++ ButterfliesHunter/Models/Butterfly.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButterfliesHunter.Models
+{
+    public class Butterfly
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Display(Name = "Butterfly name")]
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(2, ErrorMessage = "The name must have at least two characters")]
+        [MaxLength(50, ErrorMessage = "Up to 50 characters")]
+        public string Name { get; set; }
+
+
+        public string Range { get; set; }
+
+        [Required]
+        public int Ranking { get; set; }
+
+        [Required]
+        public bool IsProtected { get; set; }
+
+        [Required]
+        public int HunterId { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ ButterfliesHunter/Models/DbButterflyService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButterfliesHunter.Models
+{
+    public class DbButterflyService : IButterflyRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DbButterflyService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Butterfly> GetButterflies()
+        {
+            return _appDbContext.Butterflies;
+        }
+
+        public Butterfly GetButterfly(int id)
+        {
+            return _appDbContext.Butterflies.FirstOrDefault(b => b.Id == id);
+        }
+    }
+}
