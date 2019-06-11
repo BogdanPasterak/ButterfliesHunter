68b3b07ef8f51e5a5abb2e714a26ec83ce163c48
COB03 <COB03@LYTC-DEV03>
2019-06-11 09:34:04 +0100

    First fly

---
+++ ButterfliesHunter/Models/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ButterfliesHunter.Models
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AppDbContext context)
+        {
+            context.AddRange(
+                new Butterfly { Name= "Flame Falconrock", Range = "Europe and Asia", Ranking = 14, AuthorId = 1, IsProtected = false, Description = "" },
+                new Butterfly { }
+                // , "Moonbeam Garlicbees", "Mercury Littlestripe", "Frostbite Flickerrose", "Flare Elmbutter", "Moonbean Cozyfall", "Jeremy Daisybug", "Ginko Pollenflash", "Midnight Birdthorn", "Tangy Oakmuddle", "Happy Lemoncliff", "Aphid Mangosplash", "Tori Cedarweather", "Flax Hazelflame", "Frostbite Treemuddle", "Aed Twinklefoot", "Bryla Bumbleplume", "Fantasia Hazellight", "Leaf Speedyvalley", "Eupherbia Tuliprose", "Venus Mapletwinkle", "Parsley Fernflash", "Tangy Lovelymint", "Tinkerbell Iceberry"
+
+            );
+            context.SaveChanges();
+        }
+    }
+}
