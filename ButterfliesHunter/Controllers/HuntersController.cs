a0aa2f6f6911af96fc2486474f60f9a2be467289
COB03 <COB03@LYTC-DEV03>
2019-06-11 12:06:17 +0100

    24 fly

--- ButterfliesHunter/Controllers/HuntersController.cs
+++ ButterfliesHunter/Controllers/HuntersController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ButterfliesHunter.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ButterfliesHunter.Controllers
 {
+    [Authorize]
     public class HuntersController : Controller
     {
         private readonly IHunterRepository _hunterRepository;
