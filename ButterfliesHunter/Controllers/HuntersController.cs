a8b4785be1aaa356f4a2abeba346abf424d65024
COB03 <COB03@LYTC-DEV03>
2019-06-13 10:14:21 +0100

    rebuild h controler

--- ButterfliesHunter/Controllers/HuntersController.cs
+++ ButterfliesHunter/Controllers/HuntersController.cs
@@ -2,38 +2,153 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using ButterfliesHunter.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ButterfliesHunter.Controllers
 {
     [Authorize]
     public class HuntersController : Controller
     {
-        private readonly IHunterRepository _hunterRepository;
+        private readonly AppDbContext _context;
 
-        public HuntersController(IHunterRepository hunterRepository)
+        public HuntersController(AppDbContext context)
         {
-            _hunterRepository = hunterRepository;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Hunters
+        public async Task<IActionResult> Index()
         {
-            var hunters = _hunterRepository.GetHunters();
-            return View(hunters);
+            return View(await _context.Hunters.ToListAsync());
         }
 
-        public IActionResult Details(int? id)
+        // GET: Hunters/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            return View(_hunterRepository.GetHunter(id??1));
-        }
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        [HttpPost]
-        public IActionResult Create([Bind("HunterId,Name,Email,Voted,Display")] Hunter hunter)
-        {
+            var hunter = await _context.Hunters
+                .FirstOrDefaultAsync(m => m.HunterId == id);
+            if (hunter == null)
+            {
+                return NotFound();
+            }
 
             return View(hunter);
+        }
+
+        // GET: Hunters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hunters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HunterId,Name,Email,Voted,Display")] Hunter hunter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hunter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hunter);
+        }
+
+        // GET: Hunters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hunter = await _context.Hunters.FindAsync(id);
+            if (hunter == null)
+            {
+                return NotFound();
+            }
+            return View(hunter);
+        }
+
+        // POST: Hunters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HunterId,Name,Email,Voted,Display")] Hunter hunter)
+        {
+            if (id != hunter.HunterId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hunter);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HunterExists(hunter.HunterId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hunter);
+        }
+
+        // GET: Hunters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hunter = await _context.Hunters
+                .FirstOrDefaultAsync(m => m.HunterId == id);
+            if (hunter == null)
+            {
+                return NotFound();
+            }
+
+            return View(hunter);
+        }
+
+        // POST: Hunters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hunter = await _context.Hunters.FindAsync(id);
+            _context.Hunters.Remove(hunter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HunterExists(int id)
+        {
+            return _context.Hunters.Any(e => e.HunterId == id);
         }
     }
 }
