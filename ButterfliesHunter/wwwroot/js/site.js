5a69cce4e2eca86d378f5dea118e77e7a67b3316
COB03 <COB03@LYTC-DEV03>
2019-06-17 15:37:20 +0100

    display img with edit

--- ButterfliesHunter/wwwroot/js/site.js
+++ ButterfliesHunter/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function myF(input) {
+    let path = input.value.trim();
+
+    //if (path != "" && imageExists(path))  console.log("OK");
+    let img = document.querySelector("#image_display");
+
+
+
+    checkImage(path,
+        function () { console.log("good"); img.src = path },
+        function () { console.log("bad"); img.src = "/img/img00.jpg"});
+}
+
+function imageExists(image_url) {
+
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+
+    return http.status != 404;
+
+}
+
+function checkImage(imageSrc, good, bad) {
+    var img = new Image();
+    img.onload = good;
+    img.onerror = bad;
+    img.src = imageSrc;
+}
